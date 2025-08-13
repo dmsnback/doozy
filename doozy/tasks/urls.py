d3718c9cdc9b8db1ab0182d6f272e4b5495e0665
Dmitry Titenkov <lt200711@yandex.ru>
2025-08-13 23:12:29 +0300

    form create

--- doozy/tasks/urls.py
+++ doozy/tasks/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('task/create/', views.task_create, name='create')
 ]
--- doozy/tasks/views.py
+++ doozy/tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from tasks.forms import TaskForm 
 from tasks.models import Task
 
 
@@ -7,4 +8,11 @@ def index(request):
     template_name = 'index.html'
     tasks = Task.objects.all()
     context = {'tasks': tasks}
+    return render(request, template_name, context)
+
+
+def task_create(request):
+    template_name = 'create_task.html'
+    form = TaskForm()
+    context = {'form': form}
     return render(request, template_name, context)
