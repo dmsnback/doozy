bb2e50d4c97b4a479336b32c3dfc45a27d22fbba
Dmitry Titenkov <lt200711@yandex.ru>
2025-08-24 21:06:06 +0300

    Доступ пользователей

--- doozy/doozy/settings.py
+++ doozy/doozy/settings.py
@@ -146,5 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
 
-LOGIN_URL = 'login' 
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'tasks:index'
