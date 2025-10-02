b648a04b8aec424bfb3cce956dfe5db83bb314ea
Dmitry Titenkov <lt200711@yandex.ru>
2025-10-02 22:50:19 +0300

    userprofile2

--- doozy/user/urls.py
+++ doozy/user/urls.py
@@ -6,4 +6,14 @@ app_name = 'user'
 
 urlpatterns = [
     path('profile/<str:username>/', views.user_profile, name='user_profile'),
+    path(
+        'profile/<str:username>/edit/',
+        views.user_profile_edit,
+        name='user_profile_edit'
+    ),
+    path(
+        'profile/<str:username>/delete/',
+        views.user_profile_delete,
+        name='user_profile_delete'
+    ),
 ]
--- doozy/user/views.py
+++ doozy/user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 from tasks.models import User
 
 
@@ -7,3 +9,17 @@ def user_profile(request, username):
     user = get_object_or_404(User.objects.all(), username=username)
     context = {'user': user}
     return render(request, template_name, context)
+
+
+@login_required
+def user_profile_edit(request, username):
+    pass
+
+
+@login_required
+def user_profile_delete(request):
+    if request.method == 'POST':
+        user = get_object_or_404(User, username=request.user)
+        logout(request)
+        user.delete()
+    return redirect('tasks:index')
